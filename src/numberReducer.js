ae2eb475b79619f1babc87eb99b1ed8895b9186f
Mohib Ismail <imohib168@gmail.com>
2020-10-16 21:35:18 +0500

    Updated Repo

--- src/numberReducer.js
+++ src/numberReducer.js
@@ -1,13 +1,15 @@
 const numberReducer = (state, action) => {
     switch (action.type) {
         case "INCREMENT":
-            return state + 1;
+            return state + action.val;
         case "DECREMENT":
-            return state - 1;
+            return state - action.val;
         case "MULTIPLY":
             return state * action.val;
         case "DEFAULT":
             return 1;
+        default:
+            throw new Error();
     }
 }
 
